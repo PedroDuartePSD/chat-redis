3d037d2fb4d077b7ad116741b387256e7d69f251
PedroDuartePSD <103776070+PedroDuartePSD@users.noreply.github.com>
2023-04-25 23:59:30 +0000

    :bug: Corrigido problemas do Socket.io e Conexão com Redis Cloud.

--- index.js
+++ index.js
@@ -7,10 +7,8 @@ const PORT = process.env.PORT || 3000;
 
 const client = redis.createClient({
   password: process.env.REDIS_PASSWORD,
-  socket: {
-      host: process.env.REDIS_HOSTNAME,
-      port: process.env.REDIS_PORT
-  }
+  host: process.env.REDIS_HOSTNAME,
+  port: process.env.REDIS_PORT
 });
 
 client.on('error', (err) => {
@@ -22,13 +20,12 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', async (socket) => {
-  await client.connect();
   console.log('Novo usuário conectado');
 
   socket.on('chat message', (msg) => {
     console.log('Mensagem recebida:', msg);
 
-    client.lPush('chat messages', msg, (err, reply) => {
+    client.lpush('chat messages', msg, (err, reply) => {
       if (err) throw err;
       console.log('Mensagem salva no Redis');
     });
@@ -36,10 +33,10 @@ io.on('connection', async (socket) => {
     io.emit('chat message', msg);
   });
 
-  client.lRange('chat messages', 0, 9, (err, messages) => {
+  client.lrange('chat messages', 0, 9, (err, messages) => {
     if (err) throw err;
     console.log('Carregando as últimas 10 mensagens:', messages);
-    socket.emit('load messages', messages.reverse());
+    socket.emit('chat message', messages.reverse());
   });
 
 });
